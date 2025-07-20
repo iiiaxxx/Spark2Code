a69b5c39db908531c2cbd088277e2c8520366631
Your Name <you@example.com>
2025-07-20 11:34:35 +0400

    initial commit

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+
+    int age = 8;
+    String name = "ahmed";
+    String civilNumber = "1920522";
+    String mobileNumber = "95050270";
+    String email = "abc@mcbs.com";
+    String address = "123,Oman,Muscat";
+    String dateOfBirth = "1-1-2000";
+    Boolean isGraduated = false;
+    String resumeLink = "http://drive.google.com/";
+    String qualification = "Bachelor";
+    String major = "CyberSecurity";
+    String university = "MCBS";
+
+
+}
